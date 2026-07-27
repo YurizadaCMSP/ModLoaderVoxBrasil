5910011540832902db213a062999f88275aa9a59
YurizadaCMSP <cisampofc@gmail.com>
2026-07-27 16:29:27 -0300

    Update release.bat

--- release.bat
+++ release.bat
@@ -1,11 +1,31 @@
 @echo off
-echo Are you sure this is a final release?
-echo This will not be tagged in the logs as a development build.
-choice /C YN /M "Continue"
+
+echo ===============================================
+echo          Mod Loader - VoxBrasil
+echo        Compilação da Versão Pública
+echo ===============================================
+echo.
+echo Você tem certeza de que deseja gerar uma versão FINAL?
+echo.
+echo Esta compilação NÃO será identificada nos logs
+echo como uma versão de desenvolvimento.
+echo.
+
+choice /C SN /M "Deseja continuar"
+
 if errorlevel 2 exit /b 1
 
+:: Habilita compilação paralela no MSVC
 set CL=/MP
+
+:: Adiciona o pdbcopy ao PATH
 set "PATH=%ProgramFiles(x86)%\Windows Kits\10\Debuggers\x86;%PATH%"
+
+:: Inicia a geração da Release
 premake5 --file=release.lua prepare --toolset=vs2022 --final-release
 
+echo.
+echo ===============================================
+echo Processo concluído.
+echo ===============================================
 pause
